f7b397cc94ac06ce6f21ff930a193bbcb95ce356
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-06-09 18:33:06 +0700

    Update KeluhanController.php

--- app/Http/Controllers/KeluhanController.php
+++ app/Http/Controllers/KeluhanController.php
@@ -31,7 +31,7 @@ class KeluhanController extends Controller
             return response()->json([
                 "status"  => 500,
                 "message" => "Terjadi kesalahan. Silakan coba lagi nanti.",
-                // "error"   => $e->getMessage() // Opsional: Hapus ini pada production untuk alasan keamanan
+                "error"   => $e->getMessage() // Opsional: Hapus ini pada production untuk alasan keamanan
             ], 500);
         }
     }
