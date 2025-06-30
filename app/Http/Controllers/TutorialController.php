81ff1aa6aecc398e3826c35c4b196cbdcde0cc6e
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-06-30 22:34:09 +0700

    update tutorial pembayaran

--- app/Http/Controllers/TutorialController.php
+++ app/Http/Controllers/TutorialController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rekening;
 use App\Models\Tutorial;
 use Illuminate\Http\Request;
 
@@ -12,11 +13,35 @@ class TutorialController extends Controller
         try{
             $data = Tutorial::select([
                 'id',
+                'urutan',
                 'jenis',
                 'teks',
             ])
+            ->orderBy('urutan', 'asc')
             ->where('jenis', 'pembayaran')
+            ->get();
+
+            $rekening = Rekening::select([
+                'ref_bank.nama AS namaBank',
+                'rekening.no AS noRekening',
+                'rekening.kode_bank AS kodeBank',
+                'rekening.atas_nama AS atasNama',
+            ])
+            ->leftJoin('ref_bank', 'ref_bank.kode', '=', 'rekening.kode_bank')
             ->first();
+
+            $teksUrutan2 = '<div>Rekening tujuannya adalah&nbsp;<br><br>'. $rekening->namaBank .' kode bank : '. $rekening->kodeBank .'&nbsp;<br>Nomor rekening : '. $rekening->noRekening .'<br>Nama pemilik rekening : '. $rekening->atasNama .'</div>';
+
+            if (!$data->contains('urutan', 2)) {
+                $data->push((object)[
+                    'id' => null,
+                    'urutan' => 2,
+                    'jenis' => 'pembayaran',
+                    'teks' => $teksUrutan2,
+                ]);
+            }
+
+            $data = $data->sortBy('urutan')->values();
 
             return response()->json([
                 "status"  => 200,
