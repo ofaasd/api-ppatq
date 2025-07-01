71c504277a37cc3e295df32329260a5738b18ae4
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-07-01 14:24:59 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -150,9 +150,9 @@ Route::middleware('auth:api')->group(function () {
 // Wali Santri
 Route::prefix('wali-santri')->group(function () {
     Route::post('/login', [WaliSantriController::class, 'login']);
-    Route::post('/logout', [WaliSantriController::class, 'logout']);
 
     Route::middleware('auth:api-siswa')->group(function () {
+        Route::post('/logout', [WaliSantriController::class, 'logout']);
         Route::get('/kesehatan/{noInduk}', [WaliSantriController::class, 'kesehatan']);
         Route::get('/ketahfidzan/{noInduk}', [WaliSantriController::class, 'ketahfidzan']);
         Route::get('/perilaku/{noInduk}', [WaliSantriController::class, 'perilaku']);
