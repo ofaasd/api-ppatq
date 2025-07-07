fe027cef0e2e2d44ab867fbe046fba9b0f1b25a4
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-07-07 20:20:08 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -34,8 +34,6 @@ use App\Http\Controllers\TutorialController;
 // Route::get('siswa/verifikasi',[\App\Http\Controllers\SiswaController::class, 'verifikasi_api']);
 // Route::post('siswa/login',[\App\Http\Controllers\SiswaController::class, 'login']);
 // Route::post('siswa/logout',[\App\Http\Controllers\SiswaController::class, 'logout']);
-// Route::post('index-pembayaran',[\App\Http\Controllers\PembayaranController::class, 'index']);
-// Route::post('pembayaran',[\App\Http\Controllers\PembayaranController::class, 'store']);
 
 // Get Santri
 Route::prefix('get')->group(function () {
