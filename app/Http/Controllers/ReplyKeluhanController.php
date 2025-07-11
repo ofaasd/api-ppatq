fd0bf28f4193f2d4198aec9d446a06b161f21eb1
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-07-12 01:03:40 +0700

    Update ReplyKeluhanController.php

--- app/Http/Controllers/ReplyKeluhanController.php
+++ app/Http/Controllers/ReplyKeluhanController.php
@@ -83,7 +83,7 @@ class ReplyKeluhanController extends Controller
             return response()->json([
                 "status"  => 500,
                 "message" => "Terjadi kesalahan. Silakan coba lagi nanti.",
-                "error"   => $e->getMessage() // Opsional: Hapus ini pada production untuk alasan keamanan
+                // "error"   => $e->getMessage() // Opsional: Hapus ini pada production untuk alasan keamanan
             ], 500);
         }
     }
@@ -141,7 +141,7 @@ class ReplyKeluhanController extends Controller
             return response()->json([
                 "status"  => 500,
                 "message" => "Terjadi kesalahan. Silakan coba lagi nanti.",
-                "error"   => $e->getMessage() // Opsional: Hapus ini pada production untuk alasan keamanan
+                // "error"   => $e->getMessage() // Opsional: Hapus ini pada production untuk alasan keamanan
             ], 500);
         }
     }
