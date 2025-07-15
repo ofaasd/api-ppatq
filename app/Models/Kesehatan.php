326ef794066d8ce26975e46fb328c3541a82e217
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-07-15 21:52:33 +0700

    Update Kesehatan.php

--- app/Models/Kesehatan.php
+++ app/Models/Kesehatan.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kesehatan extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     
     protected $dateFormat = 'U';
 
