4562a0fd5a45594256af3572273de7d3884e445d
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-07-23 20:34:50 +0700

    update abort autentikasi

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,9 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if ($request->expectsJson()) {
+            abort(response()->json(['message' => 'Silahkan log in terlebih dahulu'], 401));
+        }
+        abort(response()->json(['message' => 'Unauthorized'], 401));
     }
 }
