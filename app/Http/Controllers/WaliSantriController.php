ade7302727fe521d3885566d6f2c18f49457526e
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-08-16 10:14:13 +0700

    Update WaliSantriController.php

--- app/Http/Controllers/WaliSantriController.php
+++ app/Http/Controllers/WaliSantriController.php
@@ -191,24 +191,24 @@ class WaliSantriController extends Controller
 
             $nowFormatted = Carbon::now()->translatedFormat('l d F Y, H:i');
 
-// $kelas = strtoupper($hasil->kelas);
+$kelas = strtoupper($hasil->kelas);
 
-// $message = "🎉 Selamat datang
+$message = "🎉 Selamat datang
 
-// Walisantri {$hasil->nama}. dari {$hasil->asalKota}, Kelas {$kelas}, Murroby {$hasil->namaMurroby}.
+Walisantri {$hasil->nama}. dari {$hasil->asalKota}, Kelas {$kelas}, Murroby {$hasil->namaMurroby}.
 
-// {$nowFormatted}
+{$nowFormatted}
 
-// Informasi lain juga dapat diakses melalui www.ppatq-rf.sch.id
-// ";
+Informasi lain juga dapat diakses melalui www.ppatq-rf.sch.id
+";
 
-//             // kirim wa
-//             $data = [
-//                 'no_wa' => $hasil->no_hp,
-//                 'pesan' => $message
-//             ];
+            // kirim wa
+            $data = [
+                'no_wa' => $hasil->no_hp,
+                'pesan' => $message
+            ];
 
-//             $sendWa = Helpers_wa::send_wa($data);
+            $sendWa = Helpers_wa::send_wa($data);
 
             return (new WaliSantriResource($hasil))->response()->setStatusCode(200);
         }else{
