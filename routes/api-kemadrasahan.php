3a525b7a86a299f30df248dd47294bb978119cd9
Dyaka Faiz <112964868+DyakaFaiz@users.noreply.github.com>
2026-01-21 20:56:10 +0700

    Update api-kemadrasahan.php

--- routes/api-kemadrasahan.php
+++ routes/api-kemadrasahan.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\KemadrasahanController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('kemadrasahan')->as('kemadrasahan')->group(function () {
+    Route::get('/get-santri/{idUser}', [KemadrasahanController::class, 'getSantri'])->name('.get-santri');
     Route::get('/get-data-mapel', [KemadrasahanController::class, 'getDataMapel'])->name('.get-data-mapel');
     Route::get('/get-data-santri/{noInduk}', [KemadrasahanController::class, 'getDataSantri'])->name('.get-data-santri');
     Route::get('/edit/{idDetail}', [KemadrasahanController::class, 'edit'])->name('.edit');
